496a16c7dfcbcaca34140e948be4539b14aafc12
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-10-04 13:00:20 +0100

    update components

--- react_js/learning_react/src/App.tsx
+++ react_js/learning_react/src/App.tsx
@@ -1,9 +1,28 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
+	const [showAlert, setShowAlert] = useState(false);
+
+	const onClick = () => {
+		setShowAlert(true);
+	};
+
+	const onClose = () => {
+		setShowAlert(false);
+	};
+
 	return (
 		<>
-			<Button />
+			{showAlert && (
+				<Alert onClick={onClose}>
+					<strong>Holy guacamole!</strong> You just clicked a button.
+				</Alert>
+			)}
+			<Button color="danger" onClick={onClick}>
+				My Button
+			</Button>
 		</>
 	);
 }
--- react_js/learning_react/src/components/Button.tsx
+++ react_js/learning_react/src/components/Button.tsx
@@ -1,8 +1,19 @@
-const Button = () => {
+interface Props {
+	children: string;
+	onClick: () => void;
+	color?: "primary" | "secondary" | "danger";
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
 	return (
 		<>
-			<button type="button" className="btn btn-primary" data-bs-toggle="button">
-				Toggle button
+			<button
+				type="button"
+				className={"btn btn-" + color}
+				data-bs-toggle="button"
+				onClick={onClick}
+			>
+				{children}
 			</button>
 		</>
 	);
