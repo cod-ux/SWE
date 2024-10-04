16488e4bdd061b554706de0a144621e03731ee18
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-10-04 16:13:51 +0100

    update final section

--- react_js/learning_react/src/App.tsx
+++ react_js/learning_react/src/App.tsx
@@ -1,9 +1,17 @@
 import Button from "./components/Button";
 import Alert from "./components/Alert";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 	const [showAlert, setShowAlert] = useState(false);
+
+	useEffect(() => {
+		console.log(showAlert);
+
+		return () => {
+			console.log("I am being cleaned up!");
+		};
+	}, [showAlert]);
 
 	const onClick = () => {
 		setShowAlert(true);
--- react_js/learning_react/src/components/Button.tsx
+++ react_js/learning_react/src/components/Button.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 interface Props {
 	children: string;
 	onClick: () => void;
