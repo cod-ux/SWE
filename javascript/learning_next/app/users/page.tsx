ac38d70e6486973ef20f5217a8b361174e1574de
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-10-06 12:40:14 +0100

    update compoonents

---
+++ javascript/learning_next/app/users/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Users {
+  id: number;
+  name: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: Users[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UserPage;
